51df8a99f1e73d9d5b8f1f2e094dbc7cd395e3b0
Samin Sarker <saminsarker05@gmail.com>
2024-07-13 21:45:55 -0400

    updated POST endpoint to execpt forms

--- app/__init__.py
+++ app/__init__.py
@@ -31,9 +31,9 @@ mydb.create_tables([TimelinePost])
 # POST endpoint to create a new timeline post
 @app.route("/api/timeline_post", methods=["POST"])
 def post_time_line_post():
-  name = request.json['name']
-  email = request.json['email']
-  content = request.json['content']
+  name = request.form['name']
+  email = request.form['email']
+  content = request.form['content']
   timeline_post = TimelinePost.create(name=name, email=email, content=content)
 
   return model_to_dict(timeline_post)
