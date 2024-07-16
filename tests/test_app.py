ee9dc0c50d2223f9c0821d38cc632d7d6e26ebd6
Jacob Angga <anggaj@sheridancollege.ca>
2024-07-16 16:14:03 -0600

    Added more testcases for timeline

--- tests/test_app.py
+++ tests/test_app.py
@@ -22,11 +22,29 @@ class AppTestCase(unittest.TestCase):
         assert '<link rel="preconnect" href="https://fonts.gstatic.com">' in html
 
     def test_timeline(self):
+        # GET testcases
         response = self.client.get("/api/timeline_post")
         assert response.status_code == 200
         assert response.is_json
         json = response.get_json()
         assert "timeline_posts" in json
         assert len(json["timeline_posts"]) == 0
-        # TODO Add more tests relating to the /api/timeline_post GET and POST apis
-        # TODO Add more tests relating to the timeline page
+
+        # POST testcases
+        testPost = {
+            "name": "john",
+            "email": "john@example.com",
+            "content": "Hello world",
+        }
+        response = self.client.post("/api/timeline_post", json=testPost)
+        assert response.status_code == 201
+        response = self.client.get("/api/timeline_post")
+        assert response.status_code == 200
+        json = response.get_json()
+        assert len(json["timeline_posts"]) > 0
+
+        # Timeline page testcases
+        html = response.get_data(as_text=True)
+        # Timeline button in navbar
+        assert '<li><a href="/timeline">Timeline</a></li>' in html
+        assert "<script>" in html
