34ad0f8bde2cebc649f4c14af351cffd936d1963
Samin Sarker <saminsarker05@gmail.com>
2024-07-29 23:43:04 -0400

    updated the redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -4,10 +4,6 @@ cd /root/Folio
 
 git fetch && git reset origin/main --hard
 
-source folio-venv/bin/activate
+docker compose -f docker-compose.prod.yml down
 
-pip install -r requirements.txt
-
-tmux new-session -d -s flask 'flask run --host=0.0.0.0'
-
-sudo systemctl restart myportfolio.service
+docker compose -f docker-compose.prod.yml up -d --build
